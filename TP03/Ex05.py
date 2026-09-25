bb6d3c66fe89e80a5a5f7e04f6c3d8290df3acd1
Telie25 <elie.decroux@reseau.eseo.fr>
2026-09-25 17:13:56 +0200

    Update Ex05.py

--- TP03/Ex05.py
+++ TP03/Ex05.py
@@ -6,12 +6,12 @@ def ajout(filename, content):
         print("error")
 
 def hanoi(n, source, target, auxiliary):
-    if n == 1:
+    if n==1:
         ajout("hanoy.txt",f"Déplacer le disque 1 de {source} vers {target}")
     else:
         hanoi(n-1, source, auxiliary, target)
         ajout("hanoy.txt",f"Déplacer le disque {n} de {source} vers {target}")
         hanoi(n-1, auxiliary, target, source)
-nombre_de_disques = 3
-ajout("hanoy.txt",f"Solution pour {nombre_de_disques} disques:")
-hanoi(nombre_de_disques, "A", "C", "B")
+n=3
+ajout("hanoy.txt",f"Solution pour {n} disques:")
+hanoi(n, "A", "C", "B")
